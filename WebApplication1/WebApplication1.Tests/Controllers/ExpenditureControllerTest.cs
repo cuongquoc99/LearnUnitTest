8392a59d9727b21165b96a23351c520d9b5dae6d
Cường Quốc Đặng <duytruong271@outlook.com>
2019-04-21 19:38:28 +0700

    CQ-Test

---
+++ WebApplication1/WebApplication1.Tests/Controllers/ExpenditureControllerTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication1.Controllers;
+using WebApplication1.Models;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApplication1.Tests.Controllers
+{
+    [TestClass]
+    public class ExpenditureControllerTest
+    {
+        [TestMethod]
+        public void TestIndex()
+        {
+            var db = new ExpendituresEntities();
+            var controller = new ExpenditureController();
+
+            var result = controller.Index() as ViewResult;
+
+            Assert.IsNotNull(result);
+            var model = result.Model as List<Expenditure>;
+            Assert.IsInstanceOfType(result.Model, typeof(List<Expenditure>));
+            Assert.AreEqual(db.Expenditures.Count(), (result.Model as List<Expenditure>).Count);
+
+
+        }
+
+        public void TestCreateG()
+        {
+            var controller = new ExpenditureController();
+
+            var result = controller.Create() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+
+        public void TestEditG()
+        {
+            var controller = new ExpenditureController();
+            var result0 = controller.Edit(0);
+            Assert.IsInstanceOfType(result0, typeof(HttpNotFoundResult));
+
+            var db = new ExpendituresEntities();
+            var item = db.Expenditures.First();
+            var result1 = controller.Edit(item.ID) as ViewResult;
+            Assert.IsNotNull(result1);
+            var model = result1.Model as Expenditure;
+            Assert.AreEqual(item.ID, model.ID);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/ExpenditureController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ExpenditureController : Controller
+    {
+        private ExpendituresEntities db = new ExpendituresEntities();
+
+        // GET: /Expenditure/
+        public ActionResult Index()
+        {
+            var model = db.Expenditures.ToList();
+            return View(model);
+        }
+
+        // GET: /Expenditure/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Expenditure expenditure = db.Expenditures.Find(id);
+            if (expenditure == null)
+            {
+                return HttpNotFound();
+            }
+            return View(expenditure);
+        }
+
+        // GET: /Expenditure/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Expenditure/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Expenditure model)
+        {
+            ValidateExpenditure(model);
+            if (ModelState.IsValid)
+            {
+                model.Date = DateTime.Now;
+                db.Expenditures.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+        private void ValidateExpenditure(Expenditure model)
+        {
+            if (model.Amount <= 0)
+                ModelState.AddModelError("Amount", "Số tiền quá ít!");
+        }
+        // GET: /Expenditure/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var model = db.Expenditures.Find(id);
+            if (model == null)
+            {
+                return HttpNotFound();
+            }
+            return View(model);
+        }
+
+        // POST: /Expenditure/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Expenditure model)
+        {
+            ValidateExpenditure(model);
+            if (ModelState.IsValid)
+            {
+                db.Entry(model).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        // GET: /Expenditure/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Expenditure expenditure = db.Expenditures.Find(id);
+            if (expenditure == null)
+            {
+                return HttpNotFound();
+            }
+            return View(expenditure);
+        }
+
+        // POST: /Expenditure/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Expenditure expenditure = db.Expenditures.Find(id);
+            db.Expenditures.Remove(expenditure);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
